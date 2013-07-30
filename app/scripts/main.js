0ed1e7438953b016929619a44e16cfe09a71003a
Josh Rose <lookingforrat@gmail.com>
2013-07-30 21:07:32 +0100

    Adding initial files

---
+++ app/scripts/main.js
@@ -0,0 +1,11 @@
+;(function ($) {
+    'use strict';
+
+    var images = [
+        '../images/jet-sky.jpg',
+        '../images/dew.jpg',
+        '../images/blurred-lines.jpg'
+    ];
+
+    $.backstretch(images, {duration: 3000, fade: 750})
+}(jQuery));
