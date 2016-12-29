c88d736ece638a6279903df20578bb82b46b0e18
Josh Rose <josh@etchapps.com>
2016-12-29 22:38:08 +0000

    refactor: Clean up service worker code

--- app/scripts/sw.js
+++ app/scripts/sw.js
@@ -1,77 +1,29 @@
-const CACHE_NAME = 'cache-v-0.0.1';
-
-const urlsToCache = [
-  '/index.html',
-  '/styles/main.css',
-  '/scripts/main.js',
-];
 
 self.addEventListener('install', (event) => {
-  // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      }),
+    caches.open('cache-v-0.0.1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles/main.css',
+        '/scripts/main.js',
+        '/fonts/JosefinSans-Regular.woff',
+      ])
+      .then(() => {
+        self.skipWaiting();
+      });
+    }),
   );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-
-        // IMPORTANT: Clone the request. A request is a stream and
-        // can only be consumed once. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response.
-        const fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
-          (res) => {
-            // Check if we received a valid response
-            if (!res || res.status !== 200 || res.type !== 'basic') {
-              return res;
-            }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            const responseToCache = res.clone();
-
-            caches.open(CACHE_NAME)
-              .then((cache) => {
-                cache.put(event.request, responseToCache);
-              });
-
-            return res;
-          },
-        );
-      }),
-    );
-});
-
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            console.log('Deleting cache');
-            return caches.delete(cacheName);
-          }
-
-          return null;
-        }),
-      );
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     }),
   );
 });
